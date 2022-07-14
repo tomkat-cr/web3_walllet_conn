bc39e90a85e8df1bb78659689ce3a1409ab826c3
Carlos J. Ramirez <tomkat_cr@yahoo.com>
2022-07-14 06:42:32 -0500

    Add: use web3 libs to connect/disconnect wallet

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
-import { connector, getLibrary } from '../config/index'
+import { getLibrary } from '../config/web3'
 import { Web3ReactProvider } from '@web3-react/core'
 
 function MyApp({ Component, pageProps }) {
-  return
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <Component {...pageProps} />
-  </Web3ReactProvider>
+  return (
+    <>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Component {...pageProps} />
+      </Web3ReactProvider>
+    </>
+  )
 }
 
 export default MyApp
